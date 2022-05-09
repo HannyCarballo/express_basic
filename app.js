8c005154862d5020a86449f88a6a56fe6c132ae8
Hanny Carballo <launchx00194@innovaccion.mc>
2022-05-09 01:17:14 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,35 @@
+// usando objeto express
+const express = require("express");
+// App de express
+const app = express();
+// Puerto en el que vamos a ver nuestra app: localhost: 3000
+const port = 3000;
+
+// path inicial, responderá a la url localhost: 3000
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+// respondiendo texto
+// localhost:3000/launchx
+app.get("/launchx", (req, res) => {
+  res.send("Bienvenido a launchx");
+});
+
+// regresando un objeto
+// localhost:3000/explorersInNode
+app.get("/explorersInNode", (req, res) => {
+  const explorer = { name: "Explorer", msg: "Hello" };
+  res.send(explorer);
+});
+
+// Query Params : Recibir parámetros por la url
+// http://localhost:3000/explorers/Carlo
+// req.params = {"explorerName":"carlo"}
+app.get("/explorers/:explorerName", (req, res) => {
+  res.send(req.params);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
